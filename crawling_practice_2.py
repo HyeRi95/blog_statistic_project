8c8f0a99aba63fddd4e97fb5234e6189b617e268
HyeRi95 <hyeli95@gmail.com>
2025-01-16 23:58:54 +0900

    네이버 블로그 크롤링 연습2

--- crawling_practice_2.py
+++ crawling_practice_2.py
@@ -3,7 +3,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # 요청할 URL
-url = 'https://m.blog.naver.com/11destiny3?tab=1'  # 여기에 실제 URL을 입력하세요.
+url = 'https://m.blog.naver.com/11destiny3?categoryNo=14&tab=1'  # 여기에 실제 URL을 입력하세요.
 
 # 헤더 추가 (브라우저처럼 보이게 하는 User-Agent)
 headers = {
@@ -17,21 +17,21 @@ response = requests.get(url, headers=headers)
 if response.status_code == 200:
     soup = BeautifulSoup(response.text, 'html.parser')
     
-    # '읽음' 수 크롤링
+    # '날짜' 수 크롤링
     count_list = []
-    count_elements = soup.find_all('a', class_='count__lCy1M')
-    for count in count_elements:
-        count_list.append(count.get_text(strip=True).split()[0])  # '33 읽음'에서 '33'만 추출
+    date_elements = soup.find_all('span', class_='time__mHZOn')
+    for date in date_elements:
+        print(date.text)
     
     # 'title' 값 크롤링
     title_list = []
-    title_elements = soup.find_all('strong', class_='title__UUn4H ell2')
+    title_elements = soup.find_all('strong', class_='title__UUn4H')
     for title in title_elements:
         title_text = title.get_text(strip=True)
         title_list.append(title_text)
     
     # 결과 출력
-    print("읽음 수:", count_list)
+    print("날짜:", count_list)
     print("제목:", title_list)
 else:
     print("페이지 요청 실패:", response.status_code)
